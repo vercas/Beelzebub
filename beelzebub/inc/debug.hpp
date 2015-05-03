96967865eb48d167343f5d6d66e7bf887eba7a00
Vercas <contact@vercas.com>
2015-05-04 01:35:58 +0200

    Added debug message macro.

--- beelzebub/inc/debug.hpp
+++ beelzebub/inc/debug.hpp
@@ -14,8 +14,13 @@ if (!(cond))                                                            \
 } while (false)
 
 //#define assert(cond, msg) Beelzebub::Debug::Assert(cond, __FILE__, __LINE__, msg)
+#define msg(...) do {                                                   \
+    if (Beelzebub::MainTerminal)                                        \
+        Beelzebub::MainTerminal->WriteFormat(__VA_ARGS__);              \
+} while (false)
 #else
 #define assert(...) do {} while(false)
+#define msg(...) do {} while(false)
 #endif
 
 namespace Beelzebub { namespace Debug
