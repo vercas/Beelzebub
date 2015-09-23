3d5f632dffbe30caec86b6df2db3181ba0c7da20
Vercas <contact@vercas.com>
2015-09-23 21:51:52 +0100

    Updated math.h
    Much nicer and C++-aware code.

--- beelzebub/inc/math.h
+++ beelzebub/inc/math.h
@@ -2,21 +2,72 @@
 
 #include <metaprogramming.h>
 
-__extern __forceinline __bland uint64_t RoundUp(uint64_t value, uint64_t step) __const;
-__extern __forceinline __bland uint64_t RoundDown(uint64_t value, uint64_t step) __const;
-__extern __forceinline __bland uint64_t RoundUpDiff(uint64_t value, uint64_t step) __const;
+#ifdef __cplusplus
 
-uint64_t RoundUp(uint64_t value, uint64_t step)
-{
-	return value + ((step - (value % step)) % step);
-}
+    #ifdef __BEELZEBUB__ARCH_X86
+        template<typename TNum1, typename TNum2>
+        __bland __forceinline __const constexpr auto RoundUp(TNum1 value, TNum2 step) -> decltype(((value + step - 1) / step) * step)
+        {
+            return ((value + step - 1) / step) * step;
+        }
+    #else
+        template<typename TNum1, typename TNum2>
+        __bland __forceinline __const constexpr auto RoundUp(TNum1 value, TNum2 step) -> decltype(value + ((step - (value % step)) % step))
+        {
+            return value + ((step - (value % step)) % step);
+        }
+    #endif
 
-uint64_t RoundDown(uint64_t value, uint64_t step)
-{
-	return value - (value & step);
-}
+    template<typename TNum1, typename TNum2>
+    __bland __forceinline __const constexpr auto RoundDown(TNum1 value, TNum2 step) -> decltype(value - (value & step))
+    {
+        return value - (value & step);
+    }
 
-uint64_t RoundUpDiff(uint64_t value, uint64_t step)
-{
-	return (step - (value % step)) % step;
-}
+    template<typename TNum1, typename TNum2>
+    __bland __forceinline __const constexpr auto RoundUpDiff(TNum1 value, TNum2 step) -> decltype((step - (value % step)) % step)
+    {
+        return (step - (value % step)) % step;
+    }
+
+#else
+
+    #ifdef __BEELZEBUB__ARCH_X86
+        __bland __forceinline constexpr uint64_t RoundUp64(uint64_t value, uint64_t step) __const
+        {
+            return ((value + step - 1) / step) * step;
+        }
+        __bland __forceinline constexpr uint32_t RoundUp32(uint32_t value, uint32_t step) __const
+        {
+            return ((value + step - 1) / step) * step;
+        }
+    #else
+        __bland __forceinline constexpr uint64_t RoundUp64(uint64_t value, uint64_t step) __const
+        {
+            return value + ((step - (value % step)) % step);
+        }
+        __bland __forceinline constexpr uint32_t RoundUp32(uint32_t value, uint32_t step) __const
+        {
+            return value + ((step - (value % step)) % step);
+        }
+    #endif
+
+    __bland __forceinline constexpr uint64_t RoundDown64(uint64_t value, uint64_t step) __const
+    {
+        return value - (value & step);
+    }
+    __bland __forceinline constexpr uint32_t RoundDown32(uint32_t value, uint32_t step) __const
+    {
+        return value - (value & step);
+    }
+
+    __bland __forceinline constexpr uint64_t RoundUpDiff64(uint64_t value, uint64_t step) __const
+    {
+        return (step - (value % step)) % step;
+    }
+    __bland __forceinline constexpr uint32_t RoundUpDiff32(uint32_t value, uint32_t step) __const
+    {
+        return (step - (value % step)) % step;
+    }
+
+#endif
