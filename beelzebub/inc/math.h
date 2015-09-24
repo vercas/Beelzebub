490f670cebe78728a0ad71293a673c998e33c0c6
Vercas <contact@vercas.com>
2015-09-24 10:14:48 +0100

    Updated math.h
    Added lotsa' functions.

--- beelzebub/inc/math.h
+++ beelzebub/inc/math.h
@@ -6,68 +6,135 @@
 
 #ifdef __BEELZEBUB__ARCH_X86
 template<typename TNum1, typename TNum2>
-__bland __forceinline __const constexpr auto RoundUp(TNum1 value, TNum2 step) -> decltype(((value + step - 1) / step) * step)
+__bland __forceinline __const constexpr auto RoundUp(const TNum1 value, const TNum2 step) -> decltype(((value + step - 1) / step) * step)
 {
     return ((value + step - 1) / step) * step;
 }
 #else
 template<typename TNum1, typename TNum2>
-__bland __forceinline __const constexpr auto RoundUp(TNum1 value, TNum2 step) -> decltype(value + ((step - (value % step)) % step))
+__bland __forceinline __const constexpr auto RoundUp(const TNum1 value, const TNum2 step) -> decltype(value + ((step - (value % step)) % step))
 {
     return value + ((step - (value % step)) % step);
 }
 #endif
 
 template<typename TNum1, typename TNum2>
-__bland __forceinline __const constexpr auto RoundDown(TNum1 value, TNum2 step) -> decltype(value - (value & step))
+__bland __forceinline __const constexpr auto RoundDown(const TNum1 value, const TNum2 step) -> decltype(value - (value & step))
 {
     return value - (value & step);
 }
 
 template<typename TNum1, typename TNum2>
-__bland __forceinline __const constexpr auto RoundUpDiff(TNum1 value, TNum2 step) -> decltype((step - (value % step)) % step)
+__bland __forceinline __const constexpr auto RoundUpDiff(const TNum1 value, const TNum2 step) -> decltype((step - (value % step)) % step)
 {
     return (step - (value % step)) % step;
 }
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+
+template<typename TNum1>
+__bland __forceinline __const constexpr TNum1 Minimum(const TNum1 & a)
+{
+    return a;
+}
+template<typename TNum1, typename TNum2>
+__bland __forceinline __const constexpr auto Minimum(const TNum1 & a, const TNum2 & b) -> decltype((a < b) ? a : b)
+{
+    return (a < b) ? a : b;
+}
+template<typename TNum1, typename TNum2, typename TNum3>
+__bland __forceinline __const constexpr auto Minimum(const TNum1 & a, const TNum2 & b, const TNum3 & c) -> decltype((a < Minimum(b, c)) ? a : Minimum(b, c))
+{
+    auto _b = Minimum(b, c);
+
+    return (a < _b) ? a : _b;
+}
+
+/*template<typename TNum1, typename ... TNumOthers>
+__bland __forceinline __const constexpr auto Minimum(const TNum1 & a, const TNumOthers & ... extras) -> decltype((a < Minimum(extras ...)) ? a : Minimum(extras ...))
+{
+    auto b = Minimum(extras ...);
+
+    return (a < b) ? a : b;
+}
+
+template<typename TNum1, typename ... TNumOthers>
+__bland __forceinline __const constexpr auto Minimum(const TNum1 & a, const TNumOthers ... extras) -> decltype((a < Minimum(extras ...)) ? a : Minimum(extras ...))
+{
+    auto b = Minimum(extras ...);
+
+    return (a < b) ? a : b;
+}//*/
+
+template<typename TNum1>
+__bland __forceinline __const constexpr TNum1 Maximum(const TNum1 & a)
+{
+    return a;
+}
+template<typename TNum1, typename TNum2>
+__bland __forceinline __const constexpr auto Maximum(const TNum1 & a, const TNum2 & b) -> decltype((a > b) ? a : b)
+{
+    return (a > b) ? a : b;
+}
+template<typename TNum1, typename TNum2, typename TNum3>
+__bland __forceinline __const constexpr auto Maximum(const TNum1 & a, const TNum2 & b, const TNum3 & c) -> decltype((a > Maximum(b, c)) ? a : Maximum(b, c))
+{
+    auto _b = Maximum(b, c);
+
+    return (a > _b) ? a : _b;
+}
+
+#pragma GCC diagnostic pop
 
 #else
 
 #ifdef __BEELZEBUB__ARCH_X86
-__bland __forceinline constexpr uint64_t RoundUp64(uint64_t value, uint64_t step) __const
+__bland __forceinline constexpr uint64_t RoundUp64(const uint64_t value, const uint64_t step) __const
 {
     return ((value + step - 1) / step) * step;
 }
-__bland __forceinline constexpr uint32_t RoundUp32(uint32_t value, uint32_t step) __const
+__bland __forceinline constexpr uint32_t RoundUp32(const uint32_t value, const uint32_t step) __const
 {
     return ((value + step - 1) / step) * step;
 }
 #else
-__bland __forceinline constexpr uint64_t RoundUp64(uint64_t value, uint64_t step) __const
+__bland __forceinline constexpr uint64_t RoundUp64(const uint64_t value, const uint64_t step) __const
 {
     return value + ((step - (value % step)) % step);
 }
-__bland __forceinline constexpr uint32_t RoundUp32(uint32_t value, uint32_t step) __const
+__bland __forceinline constexpr uint32_t RoundUp32(const uint32_t value, const uint32_t step) __const
 {
     return value + ((step - (value % step)) % step);
 }
 #endif
 
-__bland __forceinline constexpr uint64_t RoundDown64(uint64_t value, uint64_t step) __const
+__bland __forceinline constexpr uint64_t RoundDown64(const uint64_t value, const uint64_t step) __const
 {
     return value - (value & step);
 }
-__bland __forceinline constexpr uint32_t RoundDown32(uint32_t value, uint32_t step) __const
+__bland __forceinline constexpr uint32_t RoundDown32(const uint32_t value, const uint32_t step) __const
 {
     return value - (value & step);
 }
 
-__bland __forceinline constexpr uint64_t RoundUpDiff64(uint64_t value, uint64_t step) __const
+__bland __forceinline constexpr uint64_t RoundUpDiff64(const uint64_t value, const uint64_t step) __const
 {
     return (step - (value % step)) % step;
 }
-__bland __forceinline constexpr uint32_t RoundUpDiff32(uint32_t value, uint32_t step) __const
+__bland __forceinline constexpr uint32_t RoundUpDiff32(const uint32_t value, const uint32_t step) __const
 {
     return (step - (value % step)) % step;
 }
+
+ #define MIN(aP, bP)             \
+   ({  __typeof__ (a) _a = (aP); \
+       __typeof__ (b) _b = (bP); \
+       _a < _b ? _a : _b;        })
+ #define MAX(aP, bP)             \
+   ({  __typeof__ (a) _a = (aP); \
+       __typeof__ (b) _b = (bP); \
+       _a > _b ? _a : _b;        })
+//  Courtesy of http://stackoverflow.com/a/3437484/485098
 
 #endif
