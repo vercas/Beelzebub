1e7f9a1bf90518dc092a5f0922c444dfcf7e8467
Vercas <contact@vercas.com>
2015-09-29 11:50:04 +0100

    Added Object Pool WIP
    Eh...

---
+++ beelzebub/inc/memory/object_allocator.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <metaprogramming.h>
+#include <handles.h>
+#include <synchronization/spinlock_uninterruptible.hpp>
+
+using namespace Beelzebub;
+using namespace Beelzebub::Synchronization;
+
+namespace Beelzebub { namespace Memory
+{
+#ifdef __BEELZEBUB_KERNEL
+    typedef SpinlockUninterruptible ObjectAllocatorLock;
+    typedef int_cookie_t ObjectAllocatorLockCookie;
+#else
+    //  To do: userland will require a mutex here.
+#endif
+
+    /** <summary>Type used to represent the index of an object.</summary> */
+    typedef uint32_t obj_ind_t;
+
+    /**
+     *  <summary>Represents the contents of a free object in the object pool.</summary>
+     */
+    struct FreeObject
+    {
+        obj_ind_t Next;
+    };
+
+    /**
+     *  <summary>Represents an area of memory where objects can be allocated.</summary>
+     */
+    struct ObjectPool
+    {
+        /*  Fields  */
+
+        obj_ind_t Capacity;  //  This should help determine the size of the pool.
+        volatile obj_ind_t FreeCount;
+        obj_ind_t FirstFreeObject;      //  Used by the allocator.
+        obj_ind_t LastFreeObject;       //  Used for enlarging, mainly.
+        //  These should be creating an alignment of up to 16 if needed.
+
+        ObjectPool * Next;
+        //ObjectPool * Previous;
+
+        //ObjectPoolStatus Status;
+        SpinlockUninterruptible PropertiesLock;
+
+        /*  Constructors  */
+
+        ObjectPool() = default;
+        ObjectPool(ObjectPool const &) = delete;
+        ObjectPool & operator =(const ObjectPool &) = delete;
+        //  Aye - no copying.
+
+        /*  Methods  */
+
+        __bland __forceinline bool Contains(const uintptr_t object, const size_t objectSize, const size_t headerSize) const
+        {
+            uintptr_t start = ((uintptr_t)this) + headerSize;
+            return object >= start && object <= (start + (this->Capacity - 1) * objectSize);
+        }
+
+        __bland __forceinline FreeObject * GetFirstFreeObject(const size_t objectSize, const size_t headerSize) const
+        {
+            return (FreeObject *)(uintptr_t)((uintptr_t)this + headerSize + this->FirstFreeObject * objectSize);
+        }
+    };
+
+    typedef Handle (*AcquirePoolFunc)(size_t objectSize, size_t headerSize, size_t minimumObjects, ObjectPool * & result);
+    typedef Handle (*EnlargePoolFunc)(size_t objectSize, size_t headerSize, size_t minimumExtraObjects, ObjectPool * pool);
+    typedef Handle (*ReleasePoolFunc)(size_t objectSize, size_t headerSize, ObjectPool * pool);
+    //  Why so many parameters? So the provider can make the best decisions! :)
+
+    /**
+     *  <summary>Manages pools of fixed-size objects.</summary>
+     */
+    class ObjectAllocator
+    {
+    public:
+
+        /*  Constructors  */
+
+        ObjectAllocator() = default;
+        ObjectAllocator(ObjectAllocator const &) = delete;
+        ObjectAllocator & operator =(const ObjectAllocator &) = delete;
+
+        __bland ObjectAllocator(const size_t objectSize, const size_t objectAlignment, AcquirePoolFunc acquirer, EnlargePoolFunc enlarger, ReleasePoolFunc releaser);
+
+        /*  Methods  */
+
+        __bland Handle AllocateObject(void * & result, size_t estimatedLeft = 1);
+        __bland Handle DeallocateObject(void * object);
+
+        /*  Parameters  */
+
+        AcquirePoolFunc AcquirePool;
+        EnlargePoolFunc EnlargePool;
+        ReleasePoolFunc ReleasePool;
+
+        const size_t ObjectSize;
+        const size_t HeaderSize;
+
+        /*  Links  */
+
+        ObjectPool * FirstPool;
+        ObjectPool * LastPool;
+
+        ObjectAllocatorLock LinkageLock;
+
+        /*  Stats  */
+
+        size_t Capacity;
+        size_t FreeCount;
+    };
+}}
---
+++ beelzebub/src/memory/object_allocator.cpp
@@ -0,0 +1,143 @@
+/**
+ *  The object allocator uses a bold strategy to achieve multiprocessing safety.
+ *  
+ *  First of all, there's the object pool.
+ *  All of its properties are going to be read and changed while it's locked.
+ *  This includes linkage, counters and free object indexes.
+ *  When a method tries to work with a pool
+ *  
+ *  Then, there's the 
+ */
+
+#include <memory/object_allocator.hpp>
+#include <math.h>
+#include <debug.hpp>
+
+using namespace Beelzebub;
+using namespace Beelzebub::Memory;
+
+/****************************
+    ObjectAllocator class
+****************************/
+
+/*  Constructors  */
+
+ObjectAllocator::ObjectAllocator(const size_t objectSize, const size_t objectAlignment, AcquirePoolFunc acquirer, EnlargePoolFunc enlarger, ReleasePoolFunc releaser)
+    : AcquirePool (acquirer)
+    , EnlargePool(enlarger)
+    , ReleasePool(releaser)
+    , ObjectSize(RoundUp(Minimum(objectSize, sizeof(FreeObject)), objectAlignment))
+    , HeaderSize(RoundUp(sizeof(ObjectPool), RoundUp(Minimum(objectSize, sizeof(FreeObject)), objectAlignment)))
+    , FirstPool(nullptr)
+    , LastPool(nullptr)
+    , LinkageLock()
+    , Capacity(0)
+    , FreeCount(0)
+{
+    //  As you can see, at least a FreeObject must fit in the object size.
+    //  Also, only the alignment of the 
+}
+
+/*  Methods  */
+
+Handle ObjectAllocator::AllocateObject(void * & result, size_t estimatedLeft)
+{
+    Handle res;
+    ObjectAllocatorLockCookie cookie;
+
+    ObjectPool * first = nullptr, * current = nullptr, * justAllocated = nullptr;
+
+    //  First, let's make sure there is a first pool.
+firstPoolCheck:
+
+    if unlikely(this->FirstPool == nullptr)
+    {
+        cookie = this->LinkageLock.Acquire();
+
+        if unlikely(this->FirstPool != nullptr)
+        {   //  Maybe another core allocated the first pool in the meantime.
+            this->LinkageLock.Release(cookie);  //  Release lock.
+
+            goto firstPoolCheck;                //  Check again.
+        }
+
+        res = this->AcquirePool(this->ObjectSize, this->HeaderSize, Minimum(estimatedLeft, 1), justAllocated);
+
+        if (!res.IsOkayResult())
+        {
+            this->LinkageLock.Release(cookie);
+
+            return res;
+        }
+
+        assert(justAllocated != nullptr
+            , "Object allocator %Xp apparently successfully acquired a pool (%H), which appears to be null!"
+            , this, res);
+
+        this->FirstPool = this->LastPool = justAllocated->Next = first = current = justAllocated;
+        //  Four fields with the same value... Eh.
+
+        current->PropertiesLock.SimplyAcquire();
+
+        this->LinkageLock.SimplyRelease();
+    }
+    else
+    {
+        first = current = this->FirstPool;
+
+        cookie = current->PropertiesLock.Acquire();
+    }
+
+    do
+    {
+        if (current->FreeCount == 0)
+        {
+            //  If the current one's full, we get the next.
+
+            ObjectPool * temp = current->Next;
+
+            if (temp == first)
+            {
+                //  THIS IS THE END! (of the chain)
+
+                current->PropertiesLock.Release(cookie);
+                //  Release the current pool and restore interrupts.
+
+                break;
+                //  Go to end of the loop.
+            }
+
+            //  Otherwise...
+
+            temp->PropertiesLock.SimplyAcquire();
+            current->PropertiesLock.SimplyRelease();
+            //  Lock the next, release the current.
+
+            current = temp;
+        }
+        else
+        {
+            current->FreeCount--;
+
+            FreeObject * obj = current->GetFirstFreeObject(this->ObjectSize, this->HeaderSize);
+            current->FirstFreeObject = obj->Next;
+        }
+    } while (current != first);
+    //  Yes, the condition here is redundant. But it exists just so the compiler doesn't think this loops forever.
+    //  Not implying that the compiler would think that, but I've met my fair share of compiler bugs which lead
+    //  to me building up this paranoia.
+
+    return res;
+}
+
+Handle ObjectAllocator::DeallocateObject(void * object)
+{
+    Handle res;
+
+    ObjectPool * first = nullptr, * current = nullptr;
+
+    if unlikely(this->FirstPool == nullptr)
+    {
+
+    }
+}
