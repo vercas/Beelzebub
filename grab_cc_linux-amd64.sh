977dabd2cb3219f26f60f8b2a56ad55349c276c3
Vercas <contact@vercas.com>
2015-10-27 16:28:59 +0000

    Updated XC grabber

--- grab_cc_linux-amd64.sh
+++ grab_cc_linux-amd64.sh
@@ -3,7 +3,13 @@
 if [ ! -v CROSSCOMPILERS_DIR ]
 then
 	export CROSSCOMPILERS_DIR="/usr/local"
+
+	echo "CROSSCOMPILERS_DIR is set to $CROSSCOMPILERS_DIR"
+else
+	echo "CROSSCOMPILERS_DIR was $CROSSCOMPILERS_DIR"
 fi
+
+mkdir -p $CROSSCOMPILERS_DIR
 
 if [ ! -d "$CROSSCOMPILERS_DIR/gcc-x86_64-elf" ]
 then
