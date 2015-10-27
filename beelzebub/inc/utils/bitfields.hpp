e8ea3ff5c5f17231b42732d77e5428d45727adc8
Vercas <contact@vercas.com>
2015-10-27 09:32:22 +0000

    Added more bitfield macros

--- beelzebub/inc/utils/bitfields.hpp
+++ beelzebub/inc/utils/bitfields.hpp
@@ -9,7 +9,7 @@
 /*  Bit properties!!1!  */
 
 #ifndef BITFIELD_FLAG_RO
-//  Creates a getter for bit-based properties   .
+//  Creates a getter for bit-based flags.
 #define BITFIELD_FLAG_RO(bitInd, name, varT, var, decB, decG)                  \
 decB inline bool MCATS(Get, name)() decG                                       \
 {                                                                              \
@@ -18,7 +18,7 @@ decB inline bool MCATS(Get, name)() decG                                       \
 #endif
 
 #ifndef BITFIELD_FLAG_RW
-//  Creates a getter and setter for bit-based properties.
+//  Creates a getter and setter for bit-based flags.
 #define BITFIELD_FLAG_RW(bitInd, name, varT, var, decB, decG)                  \
 decB inline bool MCATS(Get, name)() decG                                       \
 {                                                                              \
@@ -54,5 +54,51 @@ decB inline bool MCATS(FetchFlip, name)()                                      \
     var ^=  _TEMP_BIT_DEFINE;                                                  \
                                                                                \
     return res;                                                                \
+}
+#endif
+
+#ifndef BITFIELD_STRM_RO
+//  Creates a getter for bit-based masked strings.
+#define BITFIELD_STRM_RO(bitInd, bitLen, fldT, name, varT, var, decB, decG)    \
+decB inline fldT MCATS(Get, name)() decG                                       \
+{                                                                              \
+    return (fldT)(var & MCATS(name, Bits));                                    \
+}
+#endif
+
+#ifndef BITFIELD_STRM_RW
+//  Creates a getter and setter for bit-based masked strings.
+#define BITFIELD_STRM_RW(bitInd, bitLen, fldT, name, varT, var, decB, decG)    \
+decB inline fldT MCATS(Get, name)() decG                                       \
+{                                                                              \
+    return (fldT)(var & MCATS(name, Bits));                                    \
+}                                                                              \
+decB inline void MCATS(Set, name)(fldT const val)                              \
+{                                                                              \
+    var = ((varT)val &  MCATS(name, Bits))                                     \
+        | (      var & ~MCATS(name, Bits));                                    \
+}
+#endif
+
+#ifndef BITFIELD_STRO_RO
+//  Creates a getter for bit-based masked & offset strings.
+#define BITFIELD_STRO_RO(bitInd, bitLen, fldT, name, varT, var, decB, decG)    \
+decB inline fldT MCATS(Get, name)() decG                                       \
+{                                                                              \
+    return (fldT)((var & MCATS(name, Bits)) >> MCATS(name, Offset));           \
+}
+#endif
+
+#ifndef BITFIELD_STRO_RW
+//  Creates a getter and setter for bit-based masked & offset strings.
+#define BITFIELD_STRO_RW(bitInd, bitLen, fldT, name, varT, var, decB, decG)    \
+decB inline fldT MCATS(Get, name)() decG                                       \
+{                                                                              \
+    return (fldT)((var & MCATS(name, Bits)) >> MCATS(name, Offset));           \
+}                                                                              \
+decB inline void MCATS(Set, name)(fldT const val)                              \
+{                                                                              \
+    var = (((varT)val << MCATS(name, Offset)) &  MCATS(name, Bits))            \
+        | (       var                         & ~MCATS(name, Bits));           \
 }
 #endif
