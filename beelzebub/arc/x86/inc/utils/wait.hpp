05e5bea5fddd3f0b98b274db37f432acf8661e47
Vercas <contact@vercas.com>
2015-11-05 03:11:35 +0000

    Added wait utility function
    It allows the BSP to wait while halted. AP support may not be available
    in the foreseeable future.

---
+++ beelzebub/arc/x86/inc/utils/wait.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <metaprogramming.h>
+
+namespace Beelzebub { namespace Utils
+{
+    /**
+     *  <summary>Waits for the specified time to pass.</summary>
+     */
+    __bland void Wait(uint64_t const microseconds);
+}}
---
+++ beelzebub/arc/x86/src/utils/wait.cpp
@@ -0,0 +1,24 @@
+#include <utils/wait.hpp>
+#include <system/timers/pit.hpp>
+#include <system/cpu_instructions.hpp>
+#include <math.h>
+
+using namespace Beelzebub;
+using namespace Beelzebub::System;
+using namespace Beelzebub::System::Timers;
+using namespace Beelzebub::Utils;
+
+void Utils::Wait(uint64_t const microseconds)
+{
+    size_t difference = RoundUp(microseconds, 10000) / 10000;
+    //  Round up to the length of a timer tick in microseconds, then get the
+    //  number of ticks.
+
+    size_t volatile counterStart = Pit::Counter;
+
+    do
+    {
+        CpuInstructions::Halt();
+    } while (Pit::Counter.Load() - counterStart < difference);
+    //  Yes, we can actually halt the CPU!
+}
