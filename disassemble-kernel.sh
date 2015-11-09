f62dad44c0c13ba941f753f2945c37f6c34042ba
Vercas <contact@vercas.com>
2015-11-09 22:50:10 +0000

    Updated kernel disassembly script

--- disassemble-kernel.sh
+++ disassemble-kernel.sh
@@ -1,1 +1,12 @@
-/usr/local/gcc-x86_64-elf/bin/x86_64-elf-objdump -M intel -CdlSw --no-show-raw-insn beelzebub/build/beelzebub.amd64.bin | less
+#!/bin/bash
+
+OBJDUMP=""
+
+if [ -v CROSSCOMPILERS_DIR ]
+then
+	OBJDUMP="$CROSSCOMPILERS_DIR/gcc-x86_64-elf/bin/x86_64-elf-objdump"
+else
+	OBJDUMP="/usr/local/gcc-x86_64-elf/bin/x86_64-elf-objdump"
+fi
+
+$OBJDUMP -M intel -CdlSw --no-show-raw-insn beelzebub/build/beelzebub.amd64.bin | less
