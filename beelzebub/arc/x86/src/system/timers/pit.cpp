5bab9c9895ee2c6bc41d27559856e3ee9bce6cca
Vercas <contact@vercas.com>
2015-11-29 20:38:32 +0000

    Updated PIT code
    Ticks now switch threads!

--- beelzebub/arc/x86/src/system/timers/pit.cpp
+++ beelzebub/arc/x86/src/system/timers/pit.cpp
@@ -39,9 +39,13 @@
 
 #include <system/timers/pit.hpp>
 #include <system/io_ports.hpp>
+#include <system/cpu.hpp>   //  Only used for task switching right now...
+#include <kernel.hpp>
 #include <debug.hpp>
+#include <_print/isr.hpp>
 
 using namespace Beelzebub;
+using namespace Beelzebub::Execution;
 using namespace Beelzebub::Synchronization;
 using namespace Beelzebub::System;
 using namespace Beelzebub::System::Timers;
@@ -59,6 +63,30 @@ Atomic<size_t> Pit::Counter {0};
 void Pit::IrqHandler(INTERRUPT_HANDLER_ARGS)
 {
     ++Counter;
+
+    if (CpuDataSetUp && Scheduling)
+    {
+        Thread * const activeThread = Cpu::GetData()->ActiveThread;
+
+        if (activeThread != nullptr && activeThread->Next != activeThread)
+        {
+            activeThread->State = *state;
+
+            // msg("PRE-SWITCH ");
+            // PrintToDebugTerminal(state);
+            // msg("%n");
+
+            // msg("(( AT=%Xp; N=%Xp; P=%Xp; BST=%B ))%n"
+            //     , activeThread, activeThread->Next, activeThread->Previous
+            //     , activeThread == &BootstrapThread);
+
+            activeThread->SwitchToNext(state);
+
+            // msg("%nPOST-SWITCH ");
+            // PrintToDebugTerminal(state);
+            // msg("%n");
+        }
+    }
 
     END_OF_INTERRUPT();
 }
