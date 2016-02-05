8f0816b7c759c7aec3446d5b84a8e6a47a424733
Alexandru-Mihai Maftei <contact@vercas.com>
2016-02-05 16:05:48 +0000

    Updated cross-compiler grabbing script
    Travis's shenanigans seem to have an issue with CloudFlare's SSL...

--- grab_xcs_linux-amd64.sh
+++ grab_xcs_linux-amd64.sh
@@ -13,9 +13,24 @@ mkdir -p $CROSSCOMPILERS_DIR
 
 if [ ! -d "$CROSSCOMPILERS_DIR/gcc-x86_64-elf" ]
 then
+	TMP="$(mktemp)"
+
 	pushd "$CROSSCOMPILERS_DIR"
 
-	$PRECMD wget "http://u.vercas.com/gcc-x86_64-elf.tar.xz" -O - | $PRECMD tar --no-same-owner -xJ
+	$PRECMD wget "http://u.vercas.com/gcc-x86_64-elf.tar.xz" -O $TMP
+	RES=$?
+	echo ""
+	echo "RESULT IS $RES"
+	echo ""
+
+	if [ $RES != 0 ]
+	then
+		$PRECMD wget "https://dl.dropboxusercontent.com/u/1217587/gcc-x86_64-elf.tar.xz" -O $TMP
+	fi
+
+	$PRECMD tar --no-same-owner -xJf $TMP
+
+	rm $TMP
 
 	popd
 fi
