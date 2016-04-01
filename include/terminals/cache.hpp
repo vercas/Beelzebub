1d60b93bbff677c1dad61fbaefe28447af45b813
Alexandru-Mihai Maftei <contact@vercas.com>
2016-04-01 04:27:32 +0100

    Working on cache terminal
    It caches its output in memory indefinitely.

---
+++ include/terminals/cache.hpp
@@ -0,0 +1,129 @@
+/*
+    Copyright (c) 2016 Alexandru-Mihai Maftei. All rights reserved.
+
+
+    Developed by: Alexandru-Mihai Maftei
+    aka Vercas
+    http://vercas.com | https://github.com/vercas/Beelzebub
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal with the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+      * Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimers.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimers in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the names of Alexandru-Mihai Maftei, Vercas, nor the names of
+        its contributors may be used to endorse or promote products derived from
+        this Software without specific prior written permission.
+
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    WITH THE SOFTWARE.
+
+    ---
+
+    You may also find the text of this license in "LICENSE.md", along with a more
+    thorough explanation regarding other files.
+*/
+
+#pragma once
+
+#include <terminals/base.hpp>
+
+namespace Beelzebub { namespace Terminals
+{
+    /**
+     *  <summary>Represents an area of memory where characters are stored.</summary>
+     */
+    struct CharPool
+    {
+        /*  Constructors  */
+
+        inline CharPool()
+            : Size( 0)
+            , Capacity(0)
+            , Next(nullptr)
+        {
+
+        }
+
+        CharPool(CharPool const &) = delete;
+        CharPool & operator =(const CharPool &) = delete;
+        //  No copying.
+
+        /*  Fields  */
+
+        uint32_t Size;
+        uint32_t Capacity;
+
+        CharPool * Next;
+    } __packed;
+
+    typedef Handle (*AcquireCharPoolFunc)(size_t minSize, size_t headerSize, CharPool * & result);
+    typedef Handle (*EnlargeCharPoolFunc)(size_t minSize, size_t headerSize, CharPool * pool);
+    typedef Handle (*ReleaseCharPoolFunc)(size_t headerSize, CharPool * pool);
+
+    /**
+     *  <summary>A terminal which caches output in memory.</summary>
+     */
+    class CacheTerminal : public TerminalBase
+    {
+        /*  Constructors  */
+
+    public:
+        inline CacheTerminal()
+            : TerminalBase( nullptr )
+            , AcquirePool(nullptr)
+            , EnlargePool(nullptr)
+            , ReleasePool(nullptr)
+            , FirstPool(nullptr)
+            , Capacity(0)
+            , Count(0)
+            , FreeCount(0)
+        {
+
+        }
+
+        CacheTerminal(AcquireCharPoolFunc const acquire
+                    , EnlargeCharPoolFunc const enlarge
+                    , ReleaseCharPoolFunc const release);
+
+        /*  Writing  */
+
+private:
+        TerminalWriteResult InternalWrite(char const * const str);
+
+public:
+        virtual TerminalWriteResult WriteUtf8(char const * const c);
+        virtual TerminalWriteResult Write(char const * const str);
+
+        /*  Flushing & Cleanup  */
+
+        TerminalWriteResult Dump(TerminalBase & target);
+        Handle Clear();
+
+        /*  Parameters  */
+
+    private:
+        AcquireCharPoolFunc AcquirePool;    //  When this is null, the cache
+        EnlargeCharPoolFunc EnlargePool;    //  terminal is invalid.
+        ReleaseCharPoolFunc ReleasePool;
+
+        /*  Fields  */
+
+        CharPool * FirstPool;
+
+        size_t Capacity, Count, FreeCount;
+    };
+}}
---
+++ libs/common/src/terminals/cache.cpp
@@ -0,0 +1,217 @@
+/*
+    Copyright (c) 2016 Alexandru-Mihai Maftei. All rights reserved.
+
+
+    Developed by: Alexandru-Mihai Maftei
+    aka Vercas
+    http://vercas.com | https://github.com/vercas/Beelzebub
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal with the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+      * Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimers.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimers in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the names of Alexandru-Mihai Maftei, Vercas, nor the names of
+        its contributors may be used to endorse or promote products derived from
+        this Software without specific prior written permission.
+
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    WITH THE SOFTWARE.
+
+    ---
+
+    You may also find the text of this license in "LICENSE.md", along with a more
+    thorough explanation regarding other files.
+*/
+
+#include <terminals/cache.hpp>
+#include <string.h>
+#include <math.h>
+
+using namespace Beelzebub;
+using namespace Beelzebub::Terminals;
+
+/*  Cache terminal descriptor  */
+
+TerminalCapabilities CacheTerminalCapabilities = {
+    true,   //  bool CanOutput;            //  Characters can be written to the terminal.
+    false,  //  bool CanInput;             //  Characters can be received from the terminal.
+    false,  //  bool CanRead;              //  Characters can be read back from the terminal's output.
+
+    false,  //  bool CanGetOutputPosition; //  Position of next output character can be retrieved.
+    false,  //  bool CanSetOutputPosition; //  Position of output characters can be set arbitrarily.
+    false,  //  bool CanPositionCursor;    //  Terminal features a positionable cursor.
+
+    false,  //  bool CanGetSize;           //  Terminal (window) size can be retrieved.
+    false,  //  bool CanSetSize;           //  Terminal (window) size can be changed.
+
+    false,  //  bool Buffered;             //  Terminal acts as a window over a buffer.
+    false,  //  bool CanGetBufferSize;     //  Buffer size can be retrieved.
+    false,  //  bool CanSetBufferSize;     //  Buffer size can be changed.
+    false,  //  bool CanPositionWindow;    //  The "window" can be positioned arbitrarily over the buffer.
+
+    false,  //  bool CanColorBackground;   //  Area behind/around output characters can be colored.
+    false,  //  bool CanColorForeground;   //  Output characters can be colored.
+    false,  //  bool FullColor;            //  32-bit BGRA, or ARGB in little endian.
+    false,  //  bool ForegroundAlpha;      //  Alpha channel of foreground color is supported. (ignored if false)
+    false,  //  bool BackgroundAlpha;      //  Alpha channel of background color is supported. (ignored if false)
+
+    false,  //  bool CanBold;              //  Output characters can be made bold.
+    false,  //  bool CanUnderline;         //  Output characters can be underlined.
+    false,  //  bool CanBlink;             //  Output characters can blink.
+
+    false,  //  bool CanGetStyle;          //  Current style settings can be retrieved.
+
+    false,  //  bool CanGetTabulatorWidth; //  Tabulator width may be retrieved.
+    false,  //  bool CanSetTabulatorWidth; //  Tabulator width may be changed.
+    
+    true,   //  bool SequentialOutput;     //  Character sequences can be output without explicit position.
+
+    false,  //  bool SupportsTitle;        //  Supports assignment of a title.
+
+    TerminalType::Serial    //  TerminalType Type;         //  The known type of the terminal.
+};
+
+/***************************
+    CacheTerminal struct
+****************************/
+
+/*  Constructors    */
+
+CacheTerminal::CacheTerminal(AcquireCharPoolFunc const acquire
+                           , EnlargeCharPoolFunc const enlarge
+                           , ReleaseCharPoolFunc const release)
+    : TerminalBase( &CacheTerminalCapabilities )
+    , AcquirePool(acquire)
+    , EnlargePool(enlarge)
+    , ReleasePool(release)
+    , FirstPool(nullptr)
+    , Capacity(0)
+    , Count(0)
+    , FreeCount(0)
+{
+
+}
+
+/*  Writing  */
+
+TerminalWriteResult CacheTerminal::InternalWrite(char const * const str)
+{
+    if unlikely(str == nullptr)
+        return {HandleResult::ArgumentNull, 0, InvalidCoordinates};
+
+    size_t const len = strlen(str);
+    size_t written = 0, left = len;
+
+    if unlikely(len == 0)
+        return {HandleResult::Okay, 0, InvalidCoordinates};
+    //  Very unlikely that an empty string was given, but a lot of useless
+    //  computation can be avoided if handled separately.
+
+    CharPool * pool = this->FirstPool, * last = nullptr;
+
+    while (pool != nullptr)
+    {
+        if (pool->Size < pool->Capacity)
+            break;
+        //  There is room in this pool.
+
+        last = pool;
+        pool = pool->Next;
+    }
+
+copyIntoPool:
+
+    if (pool != nullptr)
+    {
+        size_t const copyCnt = Minimum(pool->Capacity - pool->Size, left);
+        char * const dst = reinterpret_cast<char *>(pool) + sizeof(CharPool) + pool->Size;
+
+        memcpy(dst, str + written, copyCnt);
+
+        pool->Size += copyCnt;
+        written += copyCnt;
+        left -= copyCnt;
+        this->Count += copyCnt;
+        this->FreeCount -= copyCnt;
+
+        last = pool;
+    }
+
+    if (left > 0)
+    {
+        //  Okay, so there are characters left to pool but there is no pool
+        //  available. Thus, the first attempt is at enlarging the last pool.
+
+        Handle res;
+
+        if (last != nullptr)
+        {
+            uint32_t const currentCapacity = last->Capacity;
+
+            res = this->EnlargePool(left, sizeof(CharPool), last);
+
+            if unlikely(res.IsOkayResult())
+            {
+                //  It's not really unlikely to succeed, but this branch will go
+                //  back, so it's best to move it out of the way, if possible.
+
+                uint32_t const capacityDiff = last->Capacity - currentCapacity;
+
+                this->Capacity += capacityDiff;
+                this->FreeCount += capacityDiff;
+
+                pool = last;
+
+                goto copyIntoPool;
+                //  Start/resume copying.
+            }
+        }
+
+        //  Perhaps enlarging failed, or there is no previous pool to enlarge.
+        //  In this case, a new pool is acquired...
+
+        res = this->AcquirePool(left, sizeof(CharPool), pool);
+
+        if unlikely(!res.IsOkayResult())
+            return {res, (uint32_t)written, InvalidCoordinates};
+        //  Could not acquire a new pool, so the characters that are left cannot
+        //  be written anywhere.
+
+        //  At this point, creating the new pool succeeded.
+
+        if (last != nullptr)
+            last->Next = pool;
+        else
+            this->FirstPool = pool;
+        //  Link it up.
+
+        goto copyIntoPool;
+        //  Start/resume copying.
+    }
+
+    return {HandleResult::Okay, (uint32_t)written, InvalidCoordinates};
+}
+
+TerminalWriteResult CacheTerminal::WriteUtf8(const char * c)
+{
+    return this->InternalWrite(c);
+}
+
+TerminalWriteResult CacheTerminal::Write(const char * const str)
+{
+    return this->InternalWrite(str);
+}
