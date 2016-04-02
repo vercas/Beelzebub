20249728b14ed8c5dca538a9d65d908b85785771
Alexandru-Mihai Maftei <contact@vercas.com>
2016-04-02 17:00:49 +0100

    Updated disassembly scripts
    They now use the newest toolchain, and added one for the runtime
    library.

--- disassemble-kernel.sh
+++ disassemble-kernel.sh
@@ -4,9 +4,9 @@ OBJDUMP=""
 
 if [ -v CROSSCOMPILERS_DIR ]
 then
-	OBJDUMP="$CROSSCOMPILERS_DIR/gcc-x86_64-elf/bin/x86_64-elf-objdump"
+	OBJDUMP="$CROSSCOMPILERS_DIR/gcc-x86_64-beelzebub/bin/x86_64-beelzebub-objdump"
 else
-	OBJDUMP="/usr/local/gcc-x86_64-elf/bin/x86_64-elf-objdump"
+	OBJDUMP="/usr/local/gcc-x86_64-beelzebub/bin/x86_64-beelzebub-objdump"
 fi
 
 $OBJDUMP -M intel -CdlSw --no-show-raw-insn beelzebub/build/beelzebub.amd64.bin | less
--- disassemble-runtime.sh
+++ disassemble-runtime.sh
@@ -9,4 +9,4 @@ else
 	OBJDUMP="/usr/local/gcc-x86_64-beelzebub/bin/x86_64-beelzebub-objdump"
 fi
 
-$OBJDUMP -M intel -CdlSw --no-show-raw-insn apps/loadtest/build/loadtest.exe | less
+$OBJDUMP -M intel -CdlSw --no-show-raw-insn libs/runtime/build/libbeelzebub.amd64.so | less
