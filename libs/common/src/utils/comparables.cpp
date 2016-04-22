a25def660b55e0db99cfc26b8656c5225d428997
Alexandru-Mihai Maftei <contact@vercas.com>
2016-04-22 08:16:45 +0100

    Fixed comparables.cpp...

--- libs/common/src/utils/comparables.cpp
+++ libs/common/src/utils/comparables.cpp
@@ -42,27 +42,93 @@
 
 namespace Beelzebub { namespace Utils
 {
-    template<> template<>
-    comp_t Comparable<int>::Compare<int>(int const & other) const
-    {
-        return static_cast<comp_t>(this->Object - other);
-    }
-    template<> template<>
-    comp_t Comparable<int>::Compare<int>(int const && other) const
-    {
-        return static_cast<comp_t>(this->Object - other);
+    #define COMP_INT_1(TThis) \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TThis>(TThis const & other) const \
+    { \
+        return static_cast<comp_t>(this->Object - other); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TThis>(TThis const && other) const \
+    { \
+        return static_cast<comp_t>(this->Object - other); \
     }
 
-    template<> template<>
-    comp_t Comparable<unsigned int>::Compare<unsigned int>(unsigned int const & other) const
-    {
-        return static_cast<comp_t>(this->Object - other);
+    #define COMP_INT_2(TThis, TOther) \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TOther>(TOther const & other) const \
+    { \
+        return static_cast<comp_t>(this->Object - other); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TOther>(TOther const && other) const \
+    { \
+        return static_cast<comp_t>(this->Object - other); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TOther>::Compare<TThis>(TThis const & other) const \
+    { \
+        return static_cast<comp_t>(this->Object - other); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TOther>::Compare<TThis>(TThis const && other) const \
+    { \
+        return static_cast<comp_t>(this->Object - other); \
     }
-    template<> template<>
-    comp_t Comparable<unsigned int>::Compare<unsigned int>(unsigned int const && other) const
-    {
-        return static_cast<comp_t>(this->Object - other);
+
+#define COMP_INT(...) GET_MACRO2(__VA_ARGS__, COMP_INT_2, COMP_INT_1)(__VA_ARGS__)
+
+    #define COMP_COVER_1(TThis) \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TThis>(TThis const & other) const \
+    { \
+        return (this->Object == other) ? 0 : ((this->Object < other) ? -1 : 1); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TThis>(TThis const && other) const \
+    { \
+        return (this->Object == other) ? 0 : ((this->Object < other) ? -1 : 1); \
     }
+
+    #define COMP_COVER_2(TThis, TOther) \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TOther>(TOther const & other) const \
+    { \
+        return (this->Object == other) ? 0 : ((this->Object < other) ? -1 : 1); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TThis>::Compare<TOther>(TOther const && other) const \
+    { \
+        return (this->Object == other) ? 0 : ((this->Object < other) ? -1 : 1); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TOther>::Compare<TThis>(TThis const & other) const \
+    { \
+        return (this->Object == other) ? 0 : ((this->Object < other) ? -1 : 1); \
+    } \
+    template<> template<> \
+    comp_t Comparable<TOther>::Compare<TThis>(TThis const && other) const \
+    { \
+        return (this->Object == other) ? 0 : ((this->Object < other) ? -1 : 1); \
+    }
+
+#define COMP_COVER(...) GET_MACRO2(__VA_ARGS__, COMP_COVER_2, COMP_COVER_1)(__VA_ARGS__)
+
+    COMP_INT(char)
+    COMP_INT(signed char)
+    COMP_INT(unsigned char)
+
+    COMP_INT(short)
+    COMP_INT(unsigned short)
+
+    COMP_INT(int)
+    COMP_INT(unsigned int)
+
+    COMP_INT(long)
+    COMP_INT(unsigned long)
+
+    COMP_COVER(long long)
+    COMP_COVER(unsigned long long)
 
     template<> template<>
     comp_t Comparable<char *>::Compare<char *>(char * const & other) const
