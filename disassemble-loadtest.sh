6b008d398d962fdc53f25a9e935f3b604826dcfd
Alexandru-Mihai Maftei <contact@vercas.com>
2016-08-23 13:11:44 +0300

    Updated disassemble-loadtest.sh
    It no longer hides raw instructions.

--- disassemble-loadtest.sh
+++ disassemble-loadtest.sh
@@ -9,4 +9,4 @@ else
 	OBJDUMP="/usr/local/gcc-x86_64-beelzebub/bin/x86_64-beelzebub-objdump"
 fi
 
-$OBJDUMP -M intel -CdlSw --no-show-raw-insn .vmake/amd64.debug/apps/loadtest/loadtest.exe | less
+$OBJDUMP -M intel -CdlSw .vmake/amd64.debug/apps/loadtest/loadtest.exe | less
