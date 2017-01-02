0a78b7a0db26356559db0e775676c9ca66d4b2bf
Alexandru-Mihai Maftei <contact@vercas.com>
2017-01-02 16:45:08 +0000

    Updated errno.h
    Using enums properly now.

--- sysheaders/common/errno.h
+++ sysheaders/common/errno.h
@@ -39,11 +39,16 @@
 
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern __thread int errno;
 
 /*- THE REST IS TAKEN FROM http://src.gnu-darwin.org/src/sys/sys/errno.h.html
  *
  * Copyright (c) 1982, 1986, 1989, 1993
+ * Alexandru-Mihai Maftei -AND-
  *  The Regents of the University of California.  All rights reserved.
  * (c) UNIX System Laboratories, Inc.
  * All or some portions of this file are derived from material licensed
@@ -79,133 +84,119 @@ extern __thread int errno;
  * $FreeBSD: src/sys/sys/errno.h,v 1.28 2005/04/02 12:33:28 das Exp $
  */
 
-#define EPERM       1       /* Operation not permitted */
-#define ENOENT      2       /* No such file or directory */
-#define ESRCH       3       /* No such process */
-#define EINTR       4       /* Interrupted system call */
-#define EIO         5       /* Input/output error */
-#define ENXIO       6       /* Device not configured */
-#define E2BIG       7       /* Argument list too long */
-#define ENOEXEC     8       /* Exec format error */
-#define EBADF       9       /* Bad file descriptor */
-#define ECHILD      10      /* No child processes */
-#define EDEADLK     11      /* Resource deadlock avoided */
-                    /* 11 was EAGAIN */
-#define ENOMEM      12      /* Cannot allocate memory */
-#define EACCES      13      /* Permission denied */
-#define EFAULT      14      /* Bad address */
+#define ENUM_ERRNO(ENUMINST) \
+    ENUMINST(EPERM           ,  1 , "Operation not permitted") \
+    ENUMINST(ENOENT          ,  2 , "No such file or directory") \
+    ENUMINST(ESRCH           ,  3 , "No such process") \
+    ENUMINST(EINTR           ,  4 , "Interrupted system call") \
+    ENUMINST(EIO             ,  5 , "Input/output error") \
+    ENUMINST(ENXIO           ,  6 , "Device not configured") \
+    ENUMINST(E2BIG           ,  7 , "Argument list too long") \
+    ENUMINST(ENOEXEC         ,  8 , "Exec format error") \
+    ENUMINST(EBADF           ,  9 , "Bad file descriptor") \
+    ENUMINST(ECHILD          ,  10, "No child processes") \
+    ENUMINST(EDEADLK         ,  11, "Resource deadlock avoided") \
+    ENUMINST(ENOMEM          ,  12, "Cannot allocate memory") \
+    ENUMINST(EACCES          ,  13, "Permission denied") \
+    ENUMINST(EFAULT          ,  14, "Bad address") \
+    ENUMINST(EBUSY           ,  16, "Device busy") \
+    ENUMINST(EEXIST          ,  17, "File exists") \
+    ENUMINST(EXDEV           ,  18, "Cross-device link") \
+    ENUMINST(ENODEV          ,  19, "Operation not supported by device") \
+    ENUMINST(ENOTDIR         ,  20, "Not a directory") \
+    ENUMINST(EISDIR          ,  21, "Is a directory") \
+    ENUMINST(EINVAL          ,  22, "Invalid argument") \
+    ENUMINST(ENFILE          ,  23, "Too many open files in system") \
+    ENUMINST(EMFILE          ,  24, "Too many open files") \
+    ENUMINST(ENOTTY          ,  25, "Inappropriate ioctl for device") \
+    ENUMINST(EFBIG           ,  27, "File too large") \
+    ENUMINST(ENOSPC          ,  28, "No space left on device") \
+    ENUMINST(ESPIPE          ,  29, "Illegal seek") \
+    ENUMINST(EROFS           ,  30, "Read-only filesystem") \
+    ENUMINST(EMLINK          ,  31, "Too many links") \
+    ENUMINST(EPIPE           ,  32, "Broken pipe") \
+    ENUMINST(EDOM            ,  33, "Numerical argument out of domain") \
+    ENUMINST(ERANGE          ,  34, "Result too large") \
+    ENUMINST(EAGAIN          ,  35, "Resource temporarily unavailable") \
+    ENUMINST(ENAMETOOLONG    ,  63, "File name too long") \
+    ENUMINST(ENOTEMPTY       ,  66, "Directory not empty") \
+    ENUMINST(ENOLCK          ,  77, "No locks available") \
+    ENUMINST(ENOSYS          ,  78, "Function not implemented") \
+    ENUMINST(EBADMSG         ,  89, "Bad message") \
+    ENUMINST(EMULTIHOP       ,  90, "Multihop attempted") \
+    ENUMINST(ENOLINK         ,  91, "Link has been severed") \
+    ENUMINST(EPROTO          ,  92, "Protocol error") \
+    ENUM_ERRNO_NONPOSIX(ENUMINST)
+
 #ifndef _POSIX_SOURCE
-#define ENOTBLK     15      /* Block device required */
+#define ENUM_ERRNO_NONPOSIX(ENUMINST) \
+    ENUMINST(ENOTBLK         ,  15, "Block device required") \
+    ENUMINST(ETXTBSY         ,  26, "Text file busy") \
+    /*ENUMINST(EWOULDBLOCK     ,  35, "Operation would block")*/ \
+    ENUMINST(EINPROGRESS     ,  36, "Operation now in progress") \
+    ENUMINST(EALREADY        ,  37, "Operation already in progress") \
+    ENUMINST(ENOTSOCK        ,  38, "Socket operation on non-socket") \
+    ENUMINST(EDESTADDRREQ    ,  39, "Destination address required") \
+    ENUMINST(EMSGSIZE        ,  40, "Message too long") \
+    ENUMINST(EPROTOTYPE      ,  41, "Protocol wrong type for socket") \
+    ENUMINST(ENOPROTOOPT     ,  42, "Protocol not available") \
+    ENUMINST(EPROTONOSUPPORT ,  43, "Protocol not supported") \
+    ENUMINST(ESOCKTNOSUPPORT ,  44, "Socket type not supported") \
+    ENUMINST(EOPNOTSUPP      ,  45, "Operation not supported") \
+    /*ENUMINST(ENOTSUP         ,  45, "Operation not supported")*/ \
+    ENUMINST(EPFNOSUPPORT    ,  46, "Protocol family not supported") \
+    ENUMINST(EAFNOSUPPORT    ,  47, "Address family not supported by protocol family") \
+    ENUMINST(EADDRINUSE      ,  48, "Address already in use") \
+    ENUMINST(EADDRNOTAVAIL   ,  49, "Can't assign requested address") \
+    ENUMINST(ENETDOWN        ,  50, "Network is down") \
+    ENUMINST(ENETUNREACH     ,  51, "Network is unreachable") \
+    ENUMINST(ENETRESET       ,  52, "Network dropped connection on reset") \
+    ENUMINST(ECONNABORTED    ,  53, "Software caused connection abort") \
+    ENUMINST(ECONNRESET      ,  54, "Connection reset by peer") \
+    ENUMINST(ENOBUFS         ,  55, "No buffer space available") \
+    ENUMINST(EISCONN         ,  56, "Socket is already connected") \
+    ENUMINST(ENOTCONN        ,  57, "Socket is not connected") \
+    ENUMINST(ESHUTDOWN       ,  58, "Can't send after socket shutdown") \
+    ENUMINST(ETOOMANYREFS    ,  59, "Too many references: can't splice") \
+    ENUMINST(ETIMEDOUT       ,  60, "Operation timed out") \
+    ENUMINST(ECONNREFUSED    ,  61, "Connection refused") \
+    ENUMINST(ELOOP           ,  62, "Too many levels of symbolic links") \
+    ENUMINST(EHOSTDOWN       ,  64, "Host is down") \
+    ENUMINST(EHOSTUNREACH    ,  65, "No route to host") \
+    ENUMINST(EPROCLIM        ,  67, "Too many processes") \
+    ENUMINST(EUSERS          ,  68, "Too many users") \
+    ENUMINST(EDQUOT          ,  69, "Disc quota exceeded") \
+    ENUMINST(ESTALE          ,  70, "Stale NFS file handle") \
+    ENUMINST(EREMOTE         ,  71, "Too many levels of remote in path") \
+    ENUMINST(EBADRPC         ,  72, "RPC struct is bad") \
+    ENUMINST(ERPCMISMATCH    ,  73, "RPC version wrong") \
+    ENUMINST(EPROGUNAVAIL    ,  74, "RPC prog. not avail") \
+    ENUMINST(EPROGMISMATCH   ,  75, "Program version wrong") \
+    ENUMINST(EPROCUNAVAIL    ,  76, "Bad procedure for program") \
+    ENUMINST(EFTYPE          ,  79, "Inappropriate file type or format") \
+    ENUMINST(EAUTH           ,  80, "Authentication error") \
+    ENUMINST(ENEEDAUTH       ,  81, "Need authenticator") \
+    ENUMINST(EIDRM           ,  82, "Identifier removed") \
+    ENUMINST(ENOMSG          ,  83, "No message of desired type") \
+    ENUMINST(EOVERFLOW       ,  84, "Value too large to be stored in data type") \
+    ENUMINST(ECANCELED       ,  85, "Operation canceled") \
+    ENUMINST(EILSEQ          ,  86, "Illegal byte sequence") \
+    ENUMINST(ENOATTR         ,  87, "Attribute not found") \
+    ENUMINST(EDOOFUS         ,  88, "Programming error") \
+    /*ENUMINST(ELAST           ,  92, "Must be equal largest errno")*/ \
+
+#else
+    #define ENUM_ERRNO_NONPOSIX(ENUMINST)    
 #endif
-#define EBUSY       16      /* Device busy */
-#define EEXIST      17      /* File exists */
-#define EXDEV       18      /* Cross-device link */
-#define ENODEV      19      /* Operation not supported by device */
-#define ENOTDIR     20      /* Not a directory */
-#define EISDIR      21      /* Is a directory */
-#define EINVAL      22      /* Invalid argument */
-#define ENFILE      23      /* Too many open files in system */
-#define EMFILE      24      /* Too many open files */
-#define ENOTTY      25      /* Inappropriate ioctl for device */
-#ifndef _POSIX_SOURCE
-#define ETXTBSY     26      /* Text file busy */
+
+enum ERRNO
+{
+#define ERRNO_VAL(n, v, s) n = v,
+    ENUM_ERRNO(ERRNO_VAL)
+#undef ERRNO_VAL
+    ENOTSUP = EOPNOTSUPP,
+};
+
+#ifdef __cplusplus
+}
 #endif
-#define EFBIG       27      /* File too large */
-#define ENOSPC      28      /* No space left on device */
-#define ESPIPE      29      /* Illegal seek */
-#define EROFS       30      /* Read-only filesystem */
-#define EMLINK      31      /* Too many links */
-#define EPIPE       32      /* Broken pipe */
-
-/* math software */
-#define EDOM        33      /* Numerical argument out of domain */
-#define ERANGE      34      /* Result too large */
-
-/* non-blocking and interrupt i/o */
-#define EAGAIN      35      /* Resource temporarily unavailable */
-#ifndef _POSIX_SOURCE
-#define EWOULDBLOCK EAGAIN      /* Operation would block */
-#define EINPROGRESS 36      /* Operation now in progress */
-#define EALREADY    37      /* Operation already in progress */
-
-/* ipc/network software -- argument errors */
-#define ENOTSOCK    38      /* Socket operation on non-socket */
-#define EDESTADDRREQ    39      /* Destination address required */
-#define EMSGSIZE    40      /* Message too long */
-#define EPROTOTYPE  41      /* Protocol wrong type for socket */
-#define ENOPROTOOPT 42      /* Protocol not available */
-#define EPROTONOSUPPORT 43      /* Protocol not supported */
-#define ESOCKTNOSUPPORT 44      /* Socket type not supported */
-#define EOPNOTSUPP  45      /* Operation not supported */
-#define ENOTSUP     EOPNOTSUPP  /* Operation not supported */
-#define EPFNOSUPPORT    46      /* Protocol family not supported */
-#define EAFNOSUPPORT    47      /* Address family not supported by protocol family */
-#define EADDRINUSE  48      /* Address already in use */
-#define EADDRNOTAVAIL   49      /* Can't assign requested address */
-
-/* ipc/network software -- operational errors */
-#define ENETDOWN    50      /* Network is down */
-#define ENETUNREACH 51      /* Network is unreachable */
-#define ENETRESET   52      /* Network dropped connection on reset */
-#define ECONNABORTED    53      /* Software caused connection abort */
-#define ECONNRESET  54      /* Connection reset by peer */
-#define ENOBUFS     55      /* No buffer space available */
-#define EISCONN     56      /* Socket is already connected */
-#define ENOTCONN    57      /* Socket is not connected */
-#define ESHUTDOWN   58      /* Can't send after socket shutdown */
-#define ETOOMANYREFS    59      /* Too many references: can't splice */
-#define ETIMEDOUT   60      /* Operation timed out */
-#define ECONNREFUSED    61      /* Connection refused */
-
-#define ELOOP       62      /* Too many levels of symbolic links */
-#endif /* _POSIX_SOURCE */
-#define ENAMETOOLONG    63      /* File name too long */
-
-/* should be rearranged */
-#ifndef _POSIX_SOURCE
-#define EHOSTDOWN   64      /* Host is down */
-#define EHOSTUNREACH    65      /* No route to host */
-#endif /* _POSIX_SOURCE */
-#define ENOTEMPTY   66      /* Directory not empty */
-
-/* quotas & mush */
-#ifndef _POSIX_SOURCE
-#define EPROCLIM    67      /* Too many processes */
-#define EUSERS      68      /* Too many users */
-#define EDQUOT      69      /* Disc quota exceeded */
-
-/* Network File System */
-#define ESTALE      70      /* Stale NFS file handle */
-#define EREMOTE     71      /* Too many levels of remote in path */
-#define EBADRPC     72      /* RPC struct is bad */
-#define ERPCMISMATCH    73      /* RPC version wrong */
-#define EPROGUNAVAIL    74      /* RPC prog. not avail */
-#define EPROGMISMATCH   75      /* Program version wrong */
-#define EPROCUNAVAIL    76      /* Bad procedure for program */
-#endif /* _POSIX_SOURCE */
-
-#define ENOLCK      77      /* No locks available */
-#define ENOSYS      78      /* Function not implemented */
-
-#ifndef _POSIX_SOURCE
-#define EFTYPE      79      /* Inappropriate file type or format */
-#define EAUTH       80      /* Authentication error */
-#define ENEEDAUTH   81      /* Need authenticator */
-#define EIDRM       82      /* Identifier removed */
-#define ENOMSG      83      /* No message of desired type */
-#define EOVERFLOW   84      /* Value too large to be stored in data type */
-#define ECANCELED   85      /* Operation canceled */
-#define EILSEQ      86      /* Illegal byte sequence */
-#define ENOATTR     87      /* Attribute not found */
-
-#define EDOOFUS     88      /* Programming error */
-#endif /* _POSIX_SOURCE */
-
-#define EBADMSG     89      /* Bad message */
-#define EMULTIHOP   90      /* Multihop attempted */
-#define ENOLINK     91      /* Link has been severed */
-#define EPROTO      92      /* Protocol error */
-
-#ifndef _POSIX_SOURCE
-#define ELAST       92      /* Must be equal largest errno */
-#endif /* _POSIX_SOURCE */
